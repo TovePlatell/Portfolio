a8da1a9c7060e8734ffaf27ecc3808e509de1c18
TovePlatell <tove.platell@bonniernews.se>
2022-04-26 22:38:14 +0200

    slideshow

--- src/app.js
+++ src/app.js
@@ -6,3 +6,17 @@ function toggleButton(){
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+
+const projectContainer = document.getElementById("project-container");
+const project =  projectContainer.getElementsByClassName("project-card");
+
+function next(){
+    projectContainer.append(project[0]);
+
+}
+
+function prev(){
+    projectContainer.prepend(project[project.length - 1]);
+    
+}
